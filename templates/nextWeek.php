003a1198e4988b9767fe0bbf2fb7beb110405dfa
ivarne <ivarne@gmail.com>
2013-08-31 20:40:02 +0200

    PHP skulle hatt named arguments

--- templates/nextWeek.php
+++ templates/nextWeek.php
@@ -4,12 +4,12 @@
 <?php foreach($events as $event):?>
   <h3><?php echo $event->getTitle('no')?></h3>
   <em><?php echo $event->getFullDate()?></em>
-  <p><?php echo $event->getFullInfo('no',"esc_raw")?></p>
+  <p><?php echo $event->getFullInfo(true,'no',"esc_raw")?></p>
 <?php endforeach;?>
 
 <h2>And for those who prefere English</h2>
 <?php foreach($events as $event):?>
   <h3><?php echo $event->getTitle('en')?></h3>
   <em><?php echo $event->getFullDate()?></em>
-  <p><?php echo $event->getFullInfo('en',"esc_raw")?></p>
+  <p><?php echo $event->getFullInfo(true,'en',"esc_raw")?></p>
 <?php endforeach;?>
